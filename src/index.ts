9a2b51837ec99507a0122a3a2065fe6591f52975
Prokud1n <vaprokudin@edu.hse.ur>
2022-10-16 12:49:28 +0400

    export Stack

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import {Structure} from "./structure";
-const jackBlack = new Structure(['name', 'lastName', 'age']);
-
-jackBlack.set('name', 'Jack');
-jackBlack.set('lastName', 'Black');
-jackBlack.set('age', 53);
-
-console.log(jackBlack.get('name'));// 'Jack'
-console.log(jackBlack.get('lastName'));// 'Jack'
-console.log(jackBlack.get('age'));// 'Jack'
-
 export { Structure } from "./structure"
 export { LinkedList } from "./linked-list";
 export { Queue } from "./queue"
 export { Dequeue } from "./dequeue";
+export { Stack } from "./stack";
